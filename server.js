668461a285e3e8c653e529f6290239f831bcc5f1
anish20004 <anishgholap2004@gmail.com>
2024-06-13 00:38:29 +0530

    Books Create/Index/New

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser=require('body-parser')
 
 const indexRouter=require('./routes/index')
 const authorRouter=require('./routes/authors')
+const bookRouter=require('./routes/books')
 
 app.set('view engine','ejs')
 app.set('views',__dirname+'/views')
@@ -26,6 +27,7 @@ db.on('open',()=> console.log('connected to mongoose'))
 
 app.use('/',indexRouter)
 app.use('/authors',authorRouter)
+app.use('/books',bookRouter)
 
 db.on('error', (error) => {
   console.error('MongoDB connection error:', error);
